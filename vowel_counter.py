8e0cd20831643c04494f99bd5853b6b18f4d44b4
Tim Putnam <timputnam@Luna.local>
2026-02-19 23:11:45 -0600

    created a script that will count the vowels in given word

---
+++ vowel_counter.py
@@ -0,0 +1,17 @@
+def count_vowels(s):
+    counter = 0
+    vowels = "aeiouy"
+    for char in s:
+        if char in vowels:
+            counter += 1
+    return counter
+
+
+
+def main():
+    word = input('Type a word: ')
+    print(count_vowels(word))
+
+
+if __name__ == "__main__":
+    main()
