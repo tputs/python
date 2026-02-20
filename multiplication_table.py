e4129a0b571461d20968c52f2adb8ebb9caa0c66
Tim Putnam <timputnam@Luna.local>
2026-02-19 22:47:23 -0600

    created a script that will print a multiplication table of a given number

---
+++ multiplication_table.py
@@ -0,0 +1,14 @@
+def multiplication_table(n):
+    for x in range(1, n+1):
+        for y in range(1, n+1):
+            print(f"{x * y:>4}",end=" ")
+        print()
+
+
+def main():
+    number = int(input('Enter a number: '))
+    multiplication_table(number)
+
+
+if __name__ == "__main__":
+    main()
