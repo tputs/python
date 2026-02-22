47768a1de599db8dbf22396999fc6ec5037c8f7e
tputs <timothy.r.putnam@gmail.com>
2026-02-22 13:45:07 -0600

    added start and end prompt to filter logs parsed by date/time range

--- log_parser.py
+++ log_parser.py
@@ -1,14 +1,26 @@
+from datetime import datetime
+
+
+
 def read_logs(filename):
     with open(filename, 'r') as file:
         content = file.readlines()
         return content
 
 
+def filter_by_time(logs, start, end):
+    filtered = []
+    for i in logs:
+        result = parse_log(i)
+        if result["Timestamp"] >= start and result["Timestamp"] <= end:
+            filtered.append(i)
+    return filtered
+
+
 def parse_log(line):
     parts = line.split()
     log_line = {
-        "Date": parts[0],
-        "Time": parts[1],
+        "Timestamp": datetime.strptime(f"{parts[0]} {parts[1]}", "%Y-%m-%d %H:%M:%S"),
         "Level": parts[2],
         "Message": " ".join(parts[3:])
     }
@@ -40,11 +52,14 @@ def print_summary(counts):
 def main():
     filename = input('Type a filename/path: ')
     logs = read_logs(filename)
-    counts = count_levels(logs)
+    start = datetime.strptime(input('Type the start time: '), "%Y-%m-%d %H:%M:%S")
+    end = datetime.strptime(input('Type the end time: '), "%Y-%m-%d %H:%M:%S")
+    filteredlog = filter_by_time(logs, start, end)
+    counts = count_levels(filteredlog)
     print("--- Log Level Counts ---")
     print_summary(counts)
     print("--- Unique Errors ---")
-    print_summary(count_errors(logs))
+    print_summary(count_errors(filteredlog))
 
 
 if __name__ == "__main__":
