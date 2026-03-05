33fbce3afa1c272b86705dc1a0bc202243a3e63f
tputs <timothy.r.putnam@gmail.com>
2026-03-04 22:41:36 -0600

    added a child class to learn how inheritance works

--- server.py
+++ server.py
@@ -36,6 +36,24 @@ class Server:
         return f"Server Hostname: {self.hostname} IP: {self.ip} Environment: {self.environment} Status: {self.status}"
 
 
+class WebServer(Server):
+    def __init__(
+        self, hostname: str, ip: str, environment: str, status: str, port: int
+    ) -> None:
+        super().__init__(hostname, ip, environment, status)
+        self.port = port
+
+    def summary(self) -> str:
+        """Returns server attributes
+
+        Args:
+            None.
+
+        Returns:
+            Server Hostname, IP, Port, Environment, Status"""
+        return f"Server Hostname: {self.hostname} IP: {self.ip} Server Port: {self.port} Environment: {self.environment} Status: {self.status}"
+
+
 if __name__ == "__main__":
     server = Server("web-01", "192.168.1.10", "production", "healthy")
     print(server.summary())
@@ -44,3 +62,7 @@ if __name__ == "__main__":
     server2 = Server("web-02", "192.168.1.11", "production", "unhealthy")
     print(server2.summary())
     print(server2.is_healthy())
+
+    web_server = WebServer("web-03", "192.168.1.12", "production", "healthy", 443)
+    print(web_server.summary())
+    print(web_server.is_healthy())
